907f55fdffa8ceffa9002dd784caae6d66b1b241
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2022-08-15 14:26:00 +0600

    Refactoring

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -3,7 +3,7 @@ import plumber from "gulp-plumber"; // Mistakes processing
 import notify from "gulp-notify"; // Messages (prompts)
 import browsersync from "browser-sync"; // Local server
 import newer from "gulp-newer"; // update cheking out, is the pic really was updated,
-import ifPlugin from "gulp-if"; // this plugin is also valuable
+import ifPlugin from "gulp-if"; // this plugin is also valuable - and this is very important
 
 // Экспортируем объект
 export const plugins = {
